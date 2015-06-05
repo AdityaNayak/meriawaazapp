680ad66960642ccc35960df1a9b7bde9fb9bfee9
Sarthak Ahuja <sarthak12088@iiitd.ac.in>
2015-06-06 03:06:21 +0530

    guneeh

--- assets/js/issues.js
+++ assets/js/issues.js
@@ -1,4 +1,8 @@
 var constituency;
+
+
+var constituencyType;
+var polyArray=[];
 var poly;
 var view=0;
 
@@ -145,6 +149,27 @@ var iconso = [
     icon5o,
     icon6o, 
 ];
+
+function createConstituencyArray(){
+	console.log("createConstituencyArray");
+	var constituencyArra=["deoli","tughlakabad","okhla","kalkaji","sangam vihar"];
+    ListItem = Parse.Object.extend("Constituency");
+    query = new Parse.Query(ListItem);
+    query.containedIn("name", constituencyArra);
+    query.find({
+          success: function(results) {
+                testing=[];
+                for (var i = 0; i < results.length; i++) { 
+                    object= results[i];
+                    testing.push(object);
+				}
+				console.log(JSON.stringify(testing));
+            },
+          error: function(error) {
+                console.log("Error: "+error.message);notify(standardErrorMessage, "error",standardErrorDuration);
+          }
+    });
+}
 
 // Sets the map on all markers in the array.
 function setAllMap(map) {
@@ -1095,6 +1120,58 @@ function getIcon(category,status){
     return myicon;
 }
 
+function plotConstituencyArray(c,n){
+      console.log("Lets Plot a Constituency Array");
+      ListItem = Parse.Object.extend("Constituency");
+	  
+      query = new Parse.Query(ListItem);
+      query.equalTo("index", c);    
+      query.find({
+            success: function(results) {
+                  console.log("Starting Plotting: "+results[0].get("name"));
+                  var Coords=[];
+                  var pints=[];
+                  var points=results[0].get("points");
+                  console.log(points.length);
+                  for (var i = 0; i < points.length; i++) { 
+                      Coords.push(new google.maps.LatLng(points[i].latitude,points[i].longitude));
+                      pints.push({x:points[i].latitude,y:points[i].longitude});
+                  }
+                  console.log("First:"+points[0].longitude);
+                  console.log("Last:"+points[points.length-1].longitude);
+                  Poly = new google.maps.Polygon({
+                    paths: Coords,
+                    strokeColor: '#0E629B',
+                    strokeOpacity: 0.8,
+                    strokeWeight: 2,
+                    constituency: results[0].get("name"),
+                    fillColor: '#0071BC',
+                    fillOpacity: 0.35
+                  });
+                  console.log("Polygon Created!");
+                  polyArray.push(Poly);
+                  Poly.setMap(map);   
+                  var i=0;
+				  console.log(n);
+				  if(n==1){
+					  
+					  setTimeout( function() {
+						  if(currentUser.get("type")=="teamMember"){
+							populateTM();
+						  }
+						  else if(currentUser.get("type")=="neta"){
+							populate();
+						  }
+						  populateTeam();
+					  }, i * 500);
+				  }				  
+              },
+            error: function(error) {
+                  console.log("Error: "+error.message);notify(standardErrorMessage, "error",standardErrorDuration);
+            }
+      });
+  }
+
 function plotConstituency(c){
       console.log("Lets Plot a Constituency");
       ListItem = Parse.Object.extend("Constituency");
@@ -1299,59 +1376,118 @@ function populate(){
                     })(marker,object));
                 }
                 else{
-                    if(google.maps.geometry.poly.containsLocation(new google.maps.LatLng(object.get('location').latitude, object.get('location').longitude), poly)==true){
-                        //Set Icon
-                        myicon=getIcon(object.get("category"),object.get("status"));
-                                      
-                        marker = new google.maps.Marker({
-                            position: {lat: object.get('location').latitude, lng: object.get('location').longitude},
-                            map: map,
-                            title: object.get('category'),
-                            content: object,
-                            icon : myicon,
-                            draggable: false,
-                            animation: google.maps.Animation.DROP
-                        });
+					if(constituencyType!=1){
+						if(google.maps.geometry.poly.containsLocation(new google.maps.LatLng(object.get('location').latitude, object.get('location').longitude), poly)==true){
+							//Set Icon
+							myicon=getIcon(object.get("category"),object.get("status"));
+										  
+							marker = new google.maps.Marker({
+								position: {lat: object.get('location').latitude, lng: object.get('location').longitude},
+								map: map,
+								title: object.get('category'),
+								content: object,
+								icon : myicon,
+								draggable: false,
+								animation: google.maps.Animation.DROP
+							});
 
-                        var d=new Date(object.createdAt);
-                        var ago=timeSince(d);
-                        var content=object.get("content");
-                        if(object.get("content").length > 50){
-                            content=object.get("content").substring(0,50)+"...";
-                        }
-                        listView.append( "<tr id='"+object.id+"' class='"+object.get('status')+"' onClick='listViewClick("+object.id.toString()+");'><td width='100'>"+(object.get('issueId')).toString()+"</td><td width='100' class='ct'>"+object.get('category')+"</td><td class='ct'>"+content+"</td><td class='ct'>"+appropriateStatus(object.get('status'))+"</td><td width='100'>"+ago+" ago</td></tr>");                        
-                        markers.push(marker);
-                        if((marker.content).get('status')=="open"){
-                        no=no+1;
-                        }
-                        if((marker.content).get('status')=="progress"){
-                            np=np+1;
-                        }
-                        if((marker.content).get('status')=="review"){
-                            nr=nr+1;
-                        }
-                        if((marker.content).get('status')=="closed"){
-                            nc=nc+1;
-                        }
-                        google.maps.event.addListener(marker, 'click', (function(marker,object) {
-                            return function() {
-                                NProgress.start();
-                                console.log("NProgress start");
-                                if(infowindow) {
-                                    infowindow.close();
-                                }
-                                infowindow = new google.maps.InfoWindow({
-                                    maxWidth: 700,
-                                    maxHeight: 900
-                                });
-                                
-                                currmarker=marker;
-                                updateCurrentMarker(currmarker);                        
-                                infowindow.setContent(currmarker.content.get('status'));
-                                
-                            }
-                        })(marker,object));
-                    }
+							var d=new Date(object.createdAt);
+							var ago=timeSince(d);
+							var content=object.get("content");
+							if(object.get("content").length > 50){
+								content=object.get("content").substring(0,50)+"...";
+							}
+							listView.append( "<tr id='"+object.id+"' class='"+object.get('status')+"' onClick='listViewClick("+object.id.toString()+");'><td width='100'>"+(object.get('issueId')).toString()+"</td><td width='100' class='ct'>"+object.get('category')+"</td><td class='ct'>"+content+"</td><td class='ct'>"+appropriateStatus(object.get('status'))+"</td><td width='100'>"+ago+" ago</td></tr>");                        
+							markers.push(marker);
+							if((marker.content).get('status')=="open"){
+							no=no+1;
+							}
+							if((marker.content).get('status')=="progress"){
+								np=np+1;
+							}
+							if((marker.content).get('status')=="review"){
+								nr=nr+1;
+							}
+							if((marker.content).get('status')=="closed"){
+								nc=nc+1;
+							}
+							google.maps.event.addListener(marker, 'click', (function(marker,object) {
+								return function() {
+									NProgress.start();
+									console.log("NProgress start");
+									if(infowindow) {
+										infowindow.close();
+									}
+									infowindow = new google.maps.InfoWindow({
+										maxWidth: 700,
+										maxHeight: 900
+									});
+									
+									currmarker=marker;
+									updateCurrentMarker(currmarker);                        
+									infowindow.setContent(currmarker.content.get('status'));
+									
+								}
+							})(marker,object));
+						}
+					}
+					else{
+						for(var j=0;j<polyArray.length;j++){
+							if(google.maps.geometry.poly.containsLocation(new google.maps.LatLng(object.get('location').latitude, object.get('location').longitude), polyArray[j])==true){
+								//Set Icon
+								myicon=getIcon(object.get("category"),object.get("status"));
+											  
+								marker = new google.maps.Marker({
+									position: {lat: object.get('location').latitude, lng: object.get('location').longitude},
+									map: map,
+									title: object.get('category'),
+									content: object,
+									icon : myicon,
+									draggable: false,
+									animation: google.maps.Animation.DROP
+								});
+
+								var d=new Date(object.createdAt);
+								var ago=timeSince(d);
+								var content=object.get("content");
+								if(object.get("content").length > 50){
+									content=object.get("content").substring(0,50)+"...";
+								}
+								listView.append( "<tr id='"+object.id+"' class='"+object.get('status')+"' onClick='listViewClick("+object.id.toString()+");'><td width='100'>"+(object.get('issueId')).toString()+"</td><td width='100' class='ct'>"+object.get('category')+"</td><td class='ct'>"+content+"</td><td class='ct'>"+appropriateStatus(object.get('status'))+"</td><td width='100'>"+ago+" ago</td></tr>");                        
+								markers.push(marker);
+								if((marker.content).get('status')=="open"){
+								no=no+1;
+								}
+								if((marker.content).get('status')=="progress"){
+									np=np+1;
+								}
+								if((marker.content).get('status')=="review"){
+									nr=nr+1;
+								}
+								if((marker.content).get('status')=="closed"){
+									nc=nc+1;
+								}
+								google.maps.event.addListener(marker, 'click', (function(marker,object) {
+									return function() {
+										NProgress.start();
+										console.log("NProgress start");
+										if(infowindow) {
+											infowindow.close();
+										}
+										infowindow = new google.maps.InfoWindow({
+											maxWidth: 700,
+											maxHeight: 900
+										});
+										
+										currmarker=marker;
+										updateCurrentMarker(currmarker);                        
+										infowindow.setContent(currmarker.content.get('status'));
+										
+									}
+								})(marker,object));
+							}
+						}
+					}
                 }
                 
              } 
@@ -1549,12 +1685,34 @@ function initializeMap(){
 			var pointer = new Parse.Object("Neta");
 			pointer.id = n.id;
 			election.equalTo("arrayNetas", pointer);
+			election.include("[constituency.constituencyArray]");
 			election.include("constituency");
 			election.find({
 				success: function(results) {
 					constituency=results[0].get("constituency");
-					plotConstituency(constituency.get("index"));
-					map.setCenter(new google.maps.LatLng(constituency.get("center").latitude, constituency.get("center").longitude));
+					constituencyType=constituency.get("type");
+					if (constituencyType==1){
+						constituencyArray=constituency.get("constituencyArray");
+						console.log(constituencyArray);
+						polyArray=[];
+						
+						for(var i=0;i<constituencyArray.length;i++){
+							miniConstituency=constituencyArray[i];
+							
+							if(i==(constituencyArray.length-1)){
+								plotConstituencyArray(miniConstituency["index"],1);
+							}
+							else{
+								plotConstituencyArray(miniConstituency["index"],0);
+							}
+							
+							
+						}
+					}
+					else{
+						plotConstituency(constituency.get("index"));
+						map.setCenter(new google.maps.LatLng(constituency.get("center").latitude, constituency.get("center").longitude));
+					}
 				},
 				error: function(error){
 					NProgress.done();
@@ -1610,11 +1768,29 @@ function initializeMap(){
 				pointer.id = n.id;
 				election.equalTo("arrayNetas", pointer);
 				election.include("constituency");
+				election.include("constituencyArray");
 				election.find({
 					success: function(results) {
 						constituency=results[0].get("constituency");
-						plotConstituency(constituency.get("index"));
-						map.setCenter(new google.maps.LatLng(constituency.get("center").latitude, constituency.get("center").longitude));
+						constituencyType=results[0].get("type");
+						if (constituencyType==1){
+							constituencyArray=results[0].get("constituencyArray");
+							for(var i=0;i<constituencyArray.length;i++){
+								miniConstituency=constituencyArray[i];
+								
+								if(i==(constituencyArray.length-1)){
+									plotConstituencyArray(miniConstituency.get("index"),1);
+								}
+								else{
+									plotConstituencyArray(miniConstituency.get("index"),0);
+								}
+								map.setCenter(new google.maps.LatLng(miniConstituency.get("center").latitude, constituency.get("center").longitude));
+							}
+						}
+						else{
+							plotConstituency(constituency.get("index"));
+							map.setCenter(new google.maps.LatLng(constituency.get("center").latitude, constituency.get("center").longitude));
+						}
 					},
 					error: function(error){
 						NProgress.done();
